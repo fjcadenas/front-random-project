c419a35b8039ed1629f8d883eaee532c0c8debf3
Fran Cadenas <fjcadenas92@gmail.com>
2021-08-06 00:29:32 +0200

    añadidos prop types

--- components/ButtonTW.jsx
+++ components/ButtonTW.jsx
@@ -1,9 +1,16 @@
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 
 const BUTTON_TEMPLATES = {
   PRIMARY: 'primary',
   SECONDARY: 'secondary',
   DANGER: 'danger',
+};
+
+const BUTTON_SIZES = {
+  SMALL: 'small',
+  MEDIUM: 'medium',
+  LARGE: 'large',
 };
 
 export const Button = ({ label, template = 'primary', size = 'medium' }) => {
@@ -13,4 +20,10 @@ export const Button = ({ label, template = 'primary', size = 'medium' }) => {
     'bg-red-400 text-white': template === BUTTON_TEMPLATES.DANGER,
   });
   return <button className={buttonClasses}>{label}</button>;
+};
+
+Button.propTypes = {
+  template: PropTypes.oneOf(Object.values(BUTTON_TEMPLATES)),
+  size: PropTypes.oneOf(Object.values(BUTTON_SIZES)),
+  label: PropTypes.string.isRequired,
 };
